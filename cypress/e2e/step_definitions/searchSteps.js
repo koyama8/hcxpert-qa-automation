469f65532a325dd5116ea529c4aa725e4898d5b2
Matheus Koyama <koyamaCRS@gmail.com>
2026-08-06 18:27:09 -0300

    test(e2e): automatiza busca e inclui suíte na pipeline

--- cypress/e2e/step_definitions/searchSteps.js
+++ cypress/e2e/step_definitions/searchSteps.js
@@ -1,1 +1,58 @@
+const {
+  Given,
+  When,
+  Then,
+} = require('@badeball/cypress-cucumber-preprocessor');
 
+const productsPage = require('../../support/page_objects/ProductsPage');
+
+const produtoExato = 'Blue Top';
+const termoParcial = 'Blue';
+const caracteresEspeciais = '@@@';
+const produtoInexistente = 'Produto Inexistente 987654321';
+
+Given('que o usuário está na página de produtos', () => {
+  productsPage.acessarPagina();
+});
+
+When('o usuário informar o nome exato de um produto existente', () => {
+  productsPage.informarTermo(produtoExato);
+});
+
+When('o usuário informar parte do nome de um produto', () => {
+  productsPage.informarTermo(termoParcial);
+});
+
+When('o usuário informar somente caracteres especiais', () => {
+  productsPage.informarTermo(caracteresEspeciais);
+});
+
+When('o usuário deixar o campo de busca vazio', () => {
+  productsPage.deixarCampoVazio();
+});
+
+When('o usuário informar o nome de um produto inexistente', () => {
+  productsPage.informarTermo(produtoInexistente);
+});
+
+When('solicitar a busca', () => {
+  productsPage.solicitarBusca();
+});
+
+Then('somente o produto correspondente ao nome informado deverá ser apresentado', () => {
+  productsPage.validarSomenteProduto(produtoExato);
+});
+
+Then('deverão ser apresentados produtos que contenham o termo informado', () => {
+  productsPage.validarProdutosComTermo(termoParcial);
+});
+
+Then('nenhum produto deverá ser apresentado e a página deverá permanecer disponível', () => {
+  productsPage.validarNenhumProduto();
+  productsPage.validarPaginaDisponivel();
+});
+
+Then('a busca não deverá ser realizada e a lista completa de produtos deverá permanecer disponível', () => {
+  productsPage.validarListaCompleta();
+  productsPage.validarPaginaDisponivel();
+});
--- cypress/support/page_objects/ProductsPage.js
+++ cypress/support/page_objects/ProductsPage.js
@@ -1,1 +1,83 @@
+class ProductsPage {
+  elements = {
+    campoBusca: () => cy.get('#search_product'),
+    botaoBuscar: () => cy.get('#submit_search'),
+    tituloProdutos: () => cy.get('.features_items h2.title'),
+    produtos: () => cy.get('.features_items > .col-sm-4'),
+    nomesProdutos: () => cy.get('.features_items > .col-sm-4 .productinfo p:visible'),
+  };
 
+  acessarPagina() {
+    cy.visit('/products');
+    this.elements.tituloProdutos()
+      .should('be.visible')
+      .and('contain.text', 'All Products');
+  }
+
+  informarTermo(termo) {
+    this.elements.campoBusca()
+      .should('be.visible')
+      .clear()
+      .type(termo);
+  }
+
+  deixarCampoVazio() {
+    this.elements.campoBusca()
+      .should('be.visible')
+      .clear()
+      .should('have.value', '');
+  }
+
+  solicitarBusca() {
+    this.elements.botaoBuscar()
+      .should('be.visible')
+      .click();
+  }
+
+  validarSomenteProduto(nomeProduto) {
+    this.elements.tituloProdutos()
+      .should('be.visible')
+      .and('contain.text', 'Searched Products');
+
+    this.elements.nomesProdutos()
+      .should('have.length', 1)
+      .and('have.text', nomeProduto);
+  }
+
+  validarProdutosComTermo(termo) {
+    this.elements.tituloProdutos()
+      .should('be.visible')
+      .and('contain.text', 'Searched Products');
+
+    this.elements.nomesProdutos()
+      .should('have.length.greaterThan', 0)
+      .each(($produto) => {
+        expect($produto.text().toLowerCase()).to.include(termo.toLowerCase());
+      });
+  }
+
+  validarNenhumProduto() {
+    this.elements.tituloProdutos()
+      .should('be.visible')
+      .and('contain.text', 'Searched Products');
+
+    cy.get('.features_items > .col-sm-4')
+      .should('not.exist');
+  }
+
+  validarListaCompleta() {
+    this.elements.tituloProdutos()
+      .should('be.visible')
+      .and('contain.text', 'All Products');
+
+    this.elements.produtos()
+      .should('have.length.greaterThan', 0);
+  }
+
+  validarPaginaDisponivel() {
+    cy.url().should('include', '/products');
+    cy.get('body').should('be.visible');
+  }
+}
+
+module.exports = new ProductsPage();
