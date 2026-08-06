07daaeba634e364a73e6aa674c06dd968e5fcc1a
Matheus Koyama <koyamaCRS@gmail.com>
2026-08-06 19:40:44 -0300

    test(e2e): conclui cenários de checkout
    Implementa fluxo completo, formulário incompleto e perda de conectividade.
    
    Inclui checkout na execução automatizada do GitHub Actions.

--- cypress/e2e/step_definitions/checkoutSteps.js
+++ cypress/e2e/step_definitions/checkoutSteps.js
@@ -1,1 +1,71 @@
+const {
+  Given,
+  When,
+  Then,
+} = require('@badeball/cypress-cucumber-preprocessor');
 
+const checkoutPage = require('../../support/page_objects/CheckoutPage');
+
+const dadosPagamento = {
+  nome: 'Teste QA',
+  numero: '4111111111111111',
+  cvc: '123',
+  mes: '12',
+  ano: '2030',
+};
+
+const obterCredenciais = () => ({
+  email: Cypress.env('email'),
+  senha: Cypress.env('senha'),
+});
+
+Given('que o usuário está autenticado e possui um produto no carrinho', () => {
+  const { email, senha } = obterCredenciais();
+  checkoutPage.prepararCarrinho(email, senha);
+});
+
+When('o usuário revisar o endereço e prosseguir para o pagamento', () => {
+  checkoutPage.acessarCheckout();
+  checkoutPage.prosseguirParaPagamento();
+});
+
+When('informar dados fictícios válidos e confirmar o pedido', () => {
+  checkoutPage.preencherPagamento(dadosPagamento);
+  checkoutPage.confirmarPedido();
+});
+
+Then('uma mensagem de pedido realizado com sucesso deverá ser apresentada', () => {
+  checkoutPage.validarPedidoRealizado();
+});
+
+Given('que o usuário está autenticado e está na página de pagamento', () => {
+  const { email, senha } = obterCredenciais();
+  checkoutPage.prepararPaginaPagamento(email, senha);
+});
+
+When('o usuário deixar campos obrigatórios do pagamento sem preenchimento',() => {
+    checkoutPage.deixarPagamentoIncompleto();
+  },
+);
+
+When('tentar confirmar o pedido', () => {
+  checkoutPage.confirmarPedido();
+});
+
+Then('o pedido não deverá ser finalizado e o usuário deverá permanecer na página de pagamento',() => {
+    checkoutPage.validarFormularioIncompleto();
+  },
+);
+
+When('o usuário informar dados fictícios válidos para o pagamento', () => {
+  checkoutPage.preencherPagamento(dadosPagamento);
+});
+
+When('a conectividade for interrompida durante a confirmação do pedido', () => {
+  checkoutPage.simularPerdaDeConectividade();
+});
+
+Then('o pedido não deverá ser confirmado e nenhuma mensagem de sucesso deverá ser apresentada',() => {
+    checkoutPage.validarPedidoNaoConfirmado();
+  },
+);
--- cypress/support/page_objects/CheckoutPage.js
+++ cypress/support/page_objects/CheckoutPage.js
@@ -1,1 +1,132 @@
+class CheckoutPage {
+  elements = {
+    emailLogin: () => cy.get('[data-qa="login-email"]'),
+    senhaLogin: () => cy.get('[data-qa="login-password"]'),
+    botaoLogin: () => cy.get('[data-qa="login-button"]'),
+    usuarioAutenticado: () => cy.contains('a', 'Logged in as'),
+    botaoAdicionarProduto: () => cy.get('.product-information button.cart'),
+    modalCarrinho: () => cy.get('#cartModal'),
+    linkVisualizarCarrinho: () => cy.get('#cartModal a[href="/view_cart"]'),
+    botaoCheckout: () => cy.contains('a', 'Proceed To Checkout'),
+    tituloEndereco: () => cy.contains('h2', 'Address Details'),
+    tituloRevisao: () => cy.contains('h2', 'Review Your Order'),
+    comentario: () => cy.get('textarea[name="message"]'),
+    botaoFazerPedido: () => cy.contains('a', 'Place Order'),
+    nomeCartao: () => cy.get('[data-qa="name-on-card"]'),
+    numeroCartao: () => cy.get('[data-qa="card-number"]'),
+    cvc: () => cy.get('[data-qa="cvc"]'),
+    mesExpiracao: () => cy.get('[data-qa="expiry-month"]'),
+    anoExpiracao: () => cy.get('[data-qa="expiry-year"]'),
+    botaoConfirmarPagamento: () => cy.get('[data-qa="pay-button"]'),
+    pedidoRealizado: () => cy.get('[data-qa="order-placed"]'),
+  };
 
+  autenticar(email, senha) {
+    cy.visit('/login');
+    this.elements.emailLogin().should('be.visible').clear().type(email);
+    this.elements.senhaLogin().should('be.visible').clear().type(senha, {
+      log: false,
+    });
+    this.elements.botaoLogin().should('be.visible').click();
+    this.elements.usuarioAutenticado().should('be.visible');
+  }
+
+  adicionarProdutoAoCarrinho() {
+    cy.visit('/product_details/1');
+    this.elements.botaoAdicionarProduto().should('be.visible').click();
+    this.elements.modalCarrinho().should('be.visible');
+    this.elements.linkVisualizarCarrinho().should('be.visible').click();
+    cy.url().should('include', '/view_cart');
+    cy.get('#cart_info_table tbody tr').should('have.length.at.least', 1);
+  }
+
+  acessarCheckout() {
+    this.elements.botaoCheckout().should('be.visible').click();
+    cy.url().should('include', '/checkout');
+    this.elements.tituloEndereco().should('be.visible');
+    this.elements.tituloRevisao().should('be.visible');
+  }
+
+  prosseguirParaPagamento() {
+    this.elements.comentario()
+      .should('be.visible')
+      .clear()
+      .type('Pedido realizado por automação de teste.');
+    this.elements.botaoFazerPedido().should('be.visible').click();
+    cy.url().should('include', '/payment');
+    this.elements.botaoConfirmarPagamento().should('be.visible');
+  }
+
+  prepararCarrinho(email, senha) {
+    this.autenticar(email, senha);
+    this.adicionarProdutoAoCarrinho();
+  }
+
+  prepararPaginaPagamento(email, senha) {
+    this.prepararCarrinho(email, senha);
+    this.acessarCheckout();
+    this.prosseguirParaPagamento();
+  }
+
+  preencherPagamento(dados) {
+    this.elements.nomeCartao().should('be.visible').clear().type(dados.nome);
+    this.elements.numeroCartao().clear().type(dados.numero, { log: false });
+    this.elements.cvc().clear().type(dados.cvc, { log: false });
+    this.elements.mesExpiracao().clear().type(dados.mes);
+    this.elements.anoExpiracao().clear().type(dados.ano);
+  }
+
+  deixarPagamentoIncompleto() {
+    this.elements.nomeCartao().should('be.visible').clear().type('Teste QA');
+    this.elements.numeroCartao().clear();
+    this.elements.cvc().clear();
+    this.elements.mesExpiracao().clear();
+    this.elements.anoExpiracao().clear();
+  }
+
+  confirmarPedido() {
+    this.elements.botaoConfirmarPagamento().should('be.visible').click();
+  }
+
+  simularPerdaDeConectividade() {
+    cy.window().then((janela) => {
+      Object.defineProperty(janela.navigator, 'onLine', {
+        configurable: true,
+        value: false,
+      });
+      janela.dispatchEvent(new janela.Event('offline'));
+    });
+
+    this.elements.botaoConfirmarPagamento().then(($botao) => {
+      const formulario = $botao.closest('form')[0];
+
+      formulario.addEventListener(
+        'submit',
+        (evento) => evento.preventDefault(),
+        { once: true },
+      );
+    });
+
+    this.confirmarPedido();
+  }
+
+  validarPedidoRealizado() {
+    cy.url().should('include', '/payment_done');
+    this.elements.pedidoRealizado().should('be.visible');
+  }
+
+  validarFormularioIncompleto() {
+    cy.url().should('include', '/payment');
+    this.elements.pedidoRealizado().should('not.exist');
+  }
+
+  validarPedidoNaoConfirmado() {
+    cy.url().should('include', '/payment');
+    this.elements.pedidoRealizado().should('not.exist');
+    cy.contains('Congratulations! Your order has been confirmed!').should(
+      'not.exist',
+    );
+  }
+}
+
+module.exports = new CheckoutPage();
